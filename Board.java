cc61ba0f1bd002412a2a00875c2ccfa4bf4de4d9
Aylar <aylarsedaei@gmail.com>
2020-04-13 14:26:11 +0430

    created the main body and methods of board
    without anything in them :D

---
+++ Board.java
@@ -0,0 +1,56 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Board
+{
+    private final char[][] board;
+    int WScore, BScore, remaining;
+    private final char boardX[] = new char[] {'A','B','C','D','E','F','G','H'};
+
+    public Board()
+    {
+      //constructor goes here
+    }
+
+    private void findPlaceableLocations()
+    {
+       
+    }
+
+    public void displayBoard()
+    {
+       
+    }
+
+    public int gameResult()
+    {
+        
+    }
+
+    public HashSet<Point> getPlaceableLocations()
+    {
+        
+    }
+
+    public void showPlaceableLocations()
+    {
+    
+    }
+
+    public void placeMove()
+    {
+    
+    }
+
+    public void updateScores()
+    {
+    
+    }
+
+    public int coordinateX()
+    {
+    
+    }
+}
+
